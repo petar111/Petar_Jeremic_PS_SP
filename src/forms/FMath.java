0b03d9c0cb1e2eacdb6ddba73f1aa5184ed2aac7
siux <siux@DESKTOP-5P7FMQT>
2019-10-11 17:34:58 +0200

    Napravljene izmene u klasi Fmath. Dodate nove funkcionalnosti.

--- src/forms/FMath.java
+++ src/forms/FMath.java
@@ -20,13 +20,19 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 /**
  *
@@ -52,6 +58,7 @@ public class FMath extends JFrame{
         txtA = new JTextField();
         txtB = new JTextField();
         txtResult = new JTextField();
+        txtWarning = new JTextField();
         
         lblA = new JLabel();
         lblB = new JLabel();
@@ -62,13 +69,18 @@ public class FMath extends JFrame{
         btnSub = new JButton();
         btnSum = new JButton();
         
-        mathOperandsAndResultPanel = new JPanel(new GridLayout(3, 2));
+        warningPane = new JScrollPane(txtWarning, JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        
+     
+        
+        mathOperandsAndResultPanel = new JPanel();
         buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         warningPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         
         // Setting form properties
         this.setBounds(10, 50, 500, 300);
         this.setContentPane(new JPanel());
+        this.setMinimumSize(new Dimension(150, 190));
         
         
         Container pane = this.getContentPane();
@@ -78,37 +90,94 @@ public class FMath extends JFrame{
         
         pane.add(mathOperandsAndResultPanel, BorderLayout.CENTER);
         pane.add(buttonsPanel, BorderLayout.SOUTH);
-        pane.add(warningPanel, BorderLayout.NORTH);
+        //pane.add(warningPanel, BorderLayout.NORTH);
+        pane.add(warningPane, BorderLayout.NORTH);
+        
+        GroupLayout mathOandRLayout = new GroupLayout(mathOperandsAndResultPanel);
+        mathOandRLayout.setAutoCreateContainerGaps(true);
+        mathOandRLayout.setAutoCreateGaps(true);
+        mathOperandsAndResultPanel.setLayout(mathOandRLayout);
+        //mathOperandsAndResultPanel.setMinimumSize(new Dimension(30, 90));
         
         
+        
+        //mathOperandsAndResultPanel has a group layout
+        
+        mathOandRLayout.setHorizontalGroup(
+                                mathOandRLayout.createSequentialGroup()
+                                        .addGroup(mathOandRLayout
+                                                .createParallelGroup()
+                                                .addComponent(lblA)
+                                                .addComponent(lblB)
+                                                .addComponent(lblResult))
+                                        .addGroup(mathOandRLayout.createParallelGroup()
+                                                .addComponent(txtA)
+                                                .addComponent(txtB)
+                                                .addComponent(txtResult)));
+                                        //.addComponent(lblA)
+                                       // .addComponent(txtA));
+        mathOandRLayout.setVerticalGroup(
+                                mathOandRLayout.createSequentialGroup()
+                                        .addGroup(mathOandRLayout.createParallelGroup()
+                                                .addComponent(lblA)
+                                                .addComponent(txtA))
+                                        .addGroup(mathOandRLayout.createParallelGroup()
+                                                .addComponent(lblB)
+                                                .addComponent(txtB))
+                                        .addGroup(mathOandRLayout.createParallelGroup()
+                                                .addComponent(lblResult)
+                                                .addComponent(txtResult))
+                                        );
+        
+        //previous version mathOperandsAndResultPanel had a grid layout
+        /*
         mathOperandsAndResultPanel.add(lblA);
         mathOperandsAndResultPanel.add(txtA);
         mathOperandsAndResultPanel.add(lblB);
         mathOperandsAndResultPanel.add(txtB);
         mathOperandsAndResultPanel.add(lblResult);
         mathOperandsAndResultPanel.add(txtResult);
+        */
+        
+        
+        
+        
+        
         
         buttonsPanel.add(btnSum);
         buttonsPanel.add(btnSub);
+       // buttonsPanel.setMinimumSize(new Dimension(110, 20));
         
-        warningPanel.add(lblWarning);
+        //warningPanel.add(lblWarning);
+        //warningPanel.add(scrollPaneWarning);
+        //warningPanel.add(lblWarning);
+       // warningPanel.setMinimumSize(new Dimension(200, 20));
+       
+        
         
         lblWarning.setForeground(Color.ORANGE);
         lblWarning.setFont(new Font("Default", Font.BOLD, 15));
+       // lblWarning.setPreferredSize(new Dimension(100, 20));
+        //lblWarning.setPreferredSize(new Dimension(100, 20));
+        
+        
         
         txtA.setBounds(40, 20, 100, 20);
-        txtA.setMaximumSize(new Dimension(100, 20));
+        txtA.setMaximumSize(new Dimension(200, 100));
+        
         
         lblA.setBounds(20, 20, 15, 20);
         lblA.setText("a: ");
         
         txtB.setBounds(40, 60, 100, 20);
+        txtB.setMaximumSize(new Dimension(200, 100));
         
         lblB.setBounds(20, 60, 15, 20);
         lblB.setText("b: ");
         
         
         txtResult.setBounds(60, 100, 100, 20);
+        txtResult.setMaximumSize(new Dimension(200, 100));
         txtResult.setEditable(false);
         
         lblResult.setBounds(15, 100, 40, 20);
@@ -120,6 +189,13 @@ public class FMath extends JFrame{
         btnSub.setBounds(100, 150, 70, 20);
         btnSub.setText("Sub");
         
+        txtWarning.setPreferredSize(new Dimension(1, 20));
+        txtWarning.setEditable(false);
+        txtWarning.setForeground(Color.ORANGE);
+        txtWarning.setFont(new Font("Default", Font.BOLD, 15));
+        txtWarning.setBackground(null);
+        
+       
         
         
         /*
@@ -162,6 +238,32 @@ public class FMath extends JFrame{
         btnSub.setBounds(100, 150, 70, 20);
         btnSub.setText("Sub");
         */
+        
+        
+        txtWarning.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                check();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                check();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                check();
+            }
+
+            private void check() {
+                if(txtWarning.getText().equals("")){
+                    txtWarning.setPreferredSize(new Dimension(1, 20));
+                }else{
+                    txtWarning.setPreferredSize(new Dimension(325, 20));
+                }
+            }
+        });
        
         txtA.addKeyListener(new KeyListener() {
             @Override
@@ -179,15 +281,15 @@ public class FMath extends JFrame{
                 String currTextA = txtA.getText().trim();
                 String currTextB = txtB.getText().trim();
                 
-                lblWarning.setText("");
+                txtWarning.setText("");
                 try {
                    Integer.parseInt(currTextA);
                    Integer.parseInt(currTextB);
                 } catch (Exception exep) {
                     if(currTextA.equals("") || currTextB.equals("")){
-                        lblWarning.setText("Warning: At least one of the fields is empty!");
+                        txtWarning.setText("Warning: At least one of the fields is empty!");
                     }else{
-                        lblWarning.setText("Warning: Some of values you typed is invalid!");
+                        txtWarning.setText("Warning: Some of values you typed is invalid!");
                     }
                     
                 }
@@ -212,15 +314,15 @@ public class FMath extends JFrame{
                 String currTextA = txtA.getText().trim();
                 String currTextB = txtB.getText().trim();
                 
-                lblWarning.setText("");
+                txtWarning.setText("");
                 try {
                    Integer.parseInt(currTextA);
                    Integer.parseInt(currTextB);
                 } catch (Exception exep) {
                     if(currTextA.equals("") || currTextB.equals("")){
-                        lblWarning.setText("Warning: At least one of the fields is empty!");
+                        txtWarning.setText("Warning: At least one of the fields is empty!");
                     }else{
-                        lblWarning.setText("Warning: Some of values you typed is invalid!");
+                        txtWarning.setText("Warning: Some of values you typed is invalid!");
                     }
                     
                 }
@@ -269,10 +371,12 @@ public class FMath extends JFrame{
     private JPanel buttonsPanel;
     private JPanel warningPanel;
     
+    private JScrollPane warningPane;
     
     private JTextField txtA;
     private JTextField txtB;
     private JTextField txtResult;
+    private JTextField txtWarning;
     
     private JLabel lblA;
     private JLabel lblB;
